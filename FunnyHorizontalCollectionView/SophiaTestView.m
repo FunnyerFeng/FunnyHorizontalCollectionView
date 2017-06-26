5042251ae2b4ea2891d6e88a00c8efc23659296d
fengzl <fengzl@youyoubaoxian.com>
2017-06-26 11:11:53 +0800

    sophai fresh

---
+++ FunnyHorizontalCollectionView/SophiaTestView.m
@@ -0,0 +1,22 @@
+//
+//  SophiaTestView.m
+//  FunnyHorizontalCollectionView
+//
+//  Created by Funny on 2017/6/26.
+//  Copyright © 2017年 Funny. All rights reserved.
+//
+
+#import "SophiaTestView.h"
+
+@implementation SophiaTestView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        NSLog(@"");;
+    }
+    return self;
+}
+
+@end
