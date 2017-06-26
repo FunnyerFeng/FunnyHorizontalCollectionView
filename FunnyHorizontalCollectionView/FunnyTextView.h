ad1647ff891996ca384d626071c34a8a08e3ccc4
fengzl <fengzl@youyoubaoxian.com>
2017-06-26 10:34:35 +0800

    funnyviewfresh

---
+++ FunnyHorizontalCollectionView/FunnyTextView.h
@@ -0,0 +1,13 @@
+//
+//  FunnyTextView.h
+//  FunnyHorizontalCollectionView
+//
+//  Created by Funny on 2017/6/26.
+//  Copyright © 2017年 Funny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunnyTextView : UIView
+
+@end
---
+++ FunnyHorizontalCollectionView/FunnyTextView.m
@@ -0,0 +1,23 @@
+//
+//  FunnyTextView.m
+//  FunnyHorizontalCollectionView
+//
+//  Created by Funny on 2017/6/26.
+//  Copyright © 2017年 Funny. All rights reserved.
+//
+
+#import "FunnyTextView.h"
+
+@implementation FunnyTextView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        NSLog(@"ceshi View");
+    }
+    return self;
+}
+
+@end
